6126f18e528cc0a73fd4f5a8f4c0759edbc12b7f
Lisowolf <minebuilder445@gmail.com>
2022-02-16 23:37:33 +0600

    Writed many methods in Graphics class

--- src/main/java/javax/microedition/lcdui/Graphics.java
+++ src/main/java/javax/microedition/lcdui/Graphics.java
@@ -1,5 +1,7 @@
 package javax.microedition.lcdui;
 
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+
 import java.awt.*;
 
 public class Graphics {
@@ -24,8 +26,97 @@ public class Graphics {
         graphics.setColor(new Color(red, green, blue));
     }
 
+    public void setColor(int RGB) {
+        graphics.setColor(new Color(RGB));
+    }
+
+    public Font getFont() {
+        return new Font(font);
+    }
+
+    public void setStrokeStyle(int style) throws NotImplementedException {
+        // TODO: write method logic
+        throw new NotImplementedException("setStrokeStyle");
+    }
+
+    public int getStrokeStyle() {
+        return font.getStyle();
+    }
+
+    public void setFont(Font font) {
+        this.font = new java.awt.Font(java.awt.Font.SANS_SERIF, font.getStyle(), font.getSize());
+    }
+
+    public int getClipX() {
+        return graphics.getClip().getBounds().x;
+    }
+
+    public int getClipY() {
+        return graphics.getClip().getBounds().y;
+    }
+
+    public int getClipWidth() {
+        return graphics.getClip().getBounds().width;
+    }
+
+    public int getClipHeight() {
+        return graphics.getClip().getBounds().height;
+    }
+
+    public void clipRect(int x, int y, int width, int height) {
+        graphics.clipRect(x, y, width, height);
+    }
+
+    public void setClip(int x, int y, int width, int height) {
+        graphics.setClip(x, y, width, height);
+    }
+
+    public void drawLine(int x1, int y1, int x2, int y2) {
+        graphics.drawLine(x1, y1, x2, y2);
+    }
+
     public void fillRect(int x, int y, int width, int height) {
         graphics.fillRect(x, y, width, height);
+    }
+
+    public void drawRect(int x, int y, int width, int height) {
+        graphics.drawRect(x, y, width, height);
+    }
+
+    public void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
+        graphics.drawRoundRect(x, y, width, height, arcWidth, arcHeight);
+    }
+
+    public void fillRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
+        graphics.fillRoundRect(x, y, width, height, arcWidth, arcHeight);
+    }
+
+    public void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+        graphics.fillArc(x, y, width, height, startAngle, arcAngle);
+    }
+
+    public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+        graphics.drawArc(x, y, width, height, startAngle, arcAngle);
+    }
+
+    public void drawString(String str, int x, int y, int anchor) {
+        // TODO: write text align logic
+        graphics.drawString(str, x, y);
+    }
+
+    public void drawSubstring(String str, int offset, int len, int x, int y, int anchor) throws NotImplementedException {
+        // TODO: write method logic
+        throw new NotImplementedException("drawSubstring");
+    }
+
+    public void drawChar(char character, int x, int y, int anchor) throws NotImplementedException {
+        // TODO: write method logic
+        throw new NotImplementedException("drawChar");
+    }
+
+    public void drawChars(char[] data, int offset, int length, int x, int y, int anchor) throws NotImplementedException {
+        // TODO: write method logic
+        throw new NotImplementedException("drawChars");
     }
 
     public void drawImage(Image img, int x, int y, int anchor) {
@@ -38,28 +129,29 @@ public class Graphics {
         graphics.drawImage(img.image, x, y, null);
     }
 
-    public void setClip(int x, int y, int width, int height) {
-        graphics.setClip(x, y, width, height);
+    public void drawRegion(Image src, int x_src, int y_src, int width, int height, int transform, int x_dest, int y_dest, int anchor) throws NotImplementedException {
+        // TODO: write method logic
+        throw new NotImplementedException("drawRegion");
     }
 
-    public void drawLine(int x1, int y1, int x2, int y2) {
-        graphics.drawLine(x1, y1, x2, y2);
+    public void copyArea(int x_src, int y_src, int width, int height, int x_dest, int y_dest, int anchor) {
+        // TODO: write align logic
+        graphics.copyArea(x_src, y_src, width, height, x_dest, y_dest);
     }
 
-    public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
-        graphics.drawArc(x, y, width, height, startAngle, arcAngle);
+    public void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+        int[] xPoints = new int[] { x1, x2, x3 };
+        int[] yPoints = new int[] { y1, y2, y3 };
+        graphics.fillPolygon(xPoints, yPoints, 3);
     }
 
-    public Font getFont() {
-        return new Font(font);
+    public void drawRGB(int[] rgbData, int offset, int scanlength, int x, int y, int width, int height, boolean processAlpha) throws NotImplementedException {
+        // TODO: write method logic
+        throw new NotImplementedException("drawRegion");
     }
 
-    public void setFont(Font font) {
-        this.font = new java.awt.Font(java.awt.Font.SANS_SERIF, font.getStyle(), font.getSize());
-    }
-
-    public void drawString(String str, int x, int y, int anchor) {
-        // TODO: text align
-        graphics.drawString(str, x, y);
+    public int getDisplayColor(int color) {
+        // TODO: it is correct?
+        return color;
     }
 }
