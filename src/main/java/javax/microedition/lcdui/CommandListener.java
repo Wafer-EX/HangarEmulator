754caf016cfe2ca0ed049d33503ecccc893bad04
Lisowolf <minebuilder445@gmail.com>
2022-02-17 00:33:42 +0600

    👉👈

--- src/main/java/javax/microedition/lcdui/CommandListener.java
+++ src/main/java/javax/microedition/lcdui/CommandListener.java
@@ -1,5 +1,5 @@
 package javax.microedition.lcdui;
 
 public interface CommandListener {
-    public abstract void commandAction(Command command, Displayable displayable);
+    public abstract void commandAction(Command c, Displayable d);
 }
