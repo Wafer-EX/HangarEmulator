a68e698d9a582d4dafa38453dc94ee141f291ddd
Lisowolf <minebuilder445@gmail.com>
2022-06-06 17:47:13 +0600

    Some fix

--- src/main/java/things/HangarFrame.java
+++ src/main/java/things/HangarFrame.java
@@ -28,6 +28,7 @@ public class HangarFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setJMenuBar(new HangarMenuBar());
+        this.addKeyListener(new HangarKeyListener());
     }
 
     public static HangarFrame getInstance() {
@@ -58,9 +59,6 @@ public class HangarFrame extends JFrame {
         }
         this.setJMenuBar(new HangarMenuBar());
         this.add(HangarPanel.getInstance());
-        if (this.getKeyListeners().length == 0) {
-            this.addKeyListener(new HangarKeyListener());
-        }
         this.pack();
         this.revalidate();
     }
