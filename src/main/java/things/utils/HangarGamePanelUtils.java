90de0d83e1f9c84209ff8fcd4d41e48dc4085766
Lisowolf <minebuilder445@gmail.com>
2022-10-06 23:58:50 +0600

    Rewrote List page

--- src/main/java/things/utils/HangarGamePanelUtils.java
+++ src/main/java/things/utils/HangarGamePanelUtils.java
@@ -74,30 +74,4 @@ public final class HangarGamePanelUtils {
         point.y = (int) ((y - position.y) / scaleFactor);
         return point;
     }
-
-    public static void displayMEList(HangarGamePanel gamePanel, List meList) {
-        var layout = new GridLayout(6, 1, 4, 4);
-        gamePanel.setLayout(layout);
-        gamePanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-
-        for (int i = 0; i < meList.size(); i++) {
-            var button = new JButton(meList.getString(i));
-            int selectedIndex = i;
-
-            button.addActionListener(e -> {
-                meList.setSelectedIndex(selectedIndex, true);
-                meList.runSelectCommand();
-            });
-            gamePanel.add(button);
-        }
-
-        if (meList.getCommands().size() > 0) {
-            gamePanel.add(new JLabel("Options:", SwingConstants.CENTER));
-            for (var command : meList.getCommands()) {
-                var button = new JButton(command.getLabel());
-                button.addActionListener(e -> meList.getCommandListener().commandAction(command, meList));
-                gamePanel.add(button);
-            }
-        }
-    }
 }
