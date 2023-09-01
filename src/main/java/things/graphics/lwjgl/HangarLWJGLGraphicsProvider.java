555f9434bbd2404cc422eb1dee4e1b1c3b170125
Lisowolf <minebuilder445@gmail.com>
2023-09-02 01:56:21 +0600

    Some improvements

--- src/main/java/things/graphics/lwjgl/HangarLWJGLGraphicsProvider.java
+++ src/main/java/things/graphics/lwjgl/HangarLWJGLGraphicsProvider.java
@@ -388,36 +388,28 @@ public class HangarLWJGLGraphicsProvider implements HangarGraphicsProvider {
 
         lwjglActions.add(() -> {
             glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-            //glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
 
-            int buffer = glGenBuffers();
-            glBindBuffer(GL_ARRAY_BUFFER, buffer);
-            glBufferData(GL_ARRAY_BUFFER, new float[] {
-                    //x,         y,          r, g, b, 1.0f, viewportWidth, viewportHeight,
-                    //x + width, y,          r, g, b, 1.0f, viewportWidth, viewportHeight,
-                    //x + width, y + height, r, g, b, 1.0f, viewportWidth, viewportHeight,
-                    //x,         y + height, r, g, b, 1.0f, viewportWidth, viewportHeight,
-                    x,         y,          r, g, b, 1.0f, 240, 320,
-                    x + width, y,          r, g, b, 1.0f, 240, 320,
-                    x + width, y + height, r, g, b, 1.0f, 240, 320,
-                    x,         y + height, r, g, b, 1.0f, 240, 320,
-            }, GL_STATIC_DRAW);
+            var vbo = new BufferObject(GL_ARRAY_BUFFER, new float[]{
+                    // 2x POSITION | 2x UV | 4x COLOR | 1x isIgnoreSprite
+                    x, y, 0, 0, r, g, b, 1, 1,
+                    x + width, y, 1, 0, r, g, b, 1, 1,
+                    x + width, y + height, 1, 1, r, g, b, 1, 1,
+                    x, y, 0, 0, r, g, b, 1, 1,
+                    x + width, y + height, 1, 1, r, g, b, 1, 1,
+                    x, y + height, 0, 1, r, g, b, 1, 1,
+            });
 
-            glEnableVertexAttribArray(0);
-            glEnableVertexAttribArray(1);
-            glEnableVertexAttribArray(2);
-            glVertexAttribPointer(0, 2, GL_FLOAT, false, 32, 0);
-            glVertexAttribPointer(1, 4, GL_FLOAT, false, 32, 8);
-            glVertexAttribPointer(2, 2, GL_FLOAT, false, 32, 24);
+            var vao = new VertexArrayObject();
+            vao.VertexAttribPointer(0, 2, GL_FLOAT, false, 9 * 4, 0);
+            vao.VertexAttribPointer(1, 2, GL_FLOAT, false, 9 * 4, 2 * 4);
+            vao.VertexAttribPointer(2, 4, GL_FLOAT, false, 9 * 4, 4 * 4);
+            vao.VertexAttribPointer(3, 1, GL_FLOAT, false, 9 * 4, 8 * 4);
 
-            glUseProgram(vectorShaderProgram.getIdentifier());
-            glDrawArrays(GL_QUADS, 0, 4);
+            spriteShaderProgram.use();
+            spriteShaderProgram.setUniform("projectionMatrix", new Matrix4f().ortho2D(0, 240, 320, 0));
 
-            glDisableVertexAttribArray(0);
-            glDisableVertexAttribArray(1);
-            glDisableVertexAttribArray(2);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
             glUseProgram(0);
-            glDeleteBuffers(buffer);
         });
     }
 
