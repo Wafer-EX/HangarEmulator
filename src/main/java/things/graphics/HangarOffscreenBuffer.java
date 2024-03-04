59f0adb66bc446a7464d036d81f4971020af148d
Wafer-EX <minebuilder445@gmail.com>
2024-03-05 03:48:15 +0600

    Some improvements

--- src/main/java/things/graphics/HangarOffscreenBuffer.java
+++ src/main/java/things/graphics/HangarOffscreenBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Kirill Lomakin
+ * Copyright 2023-2024 Wafer EX
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,13 @@
 package things.graphics;
 
 public interface HangarOffscreenBuffer {
-    public HangarGraphicsProvider getGraphicsProvider();
+    HangarGraphicsProvider getGraphicsProvider();
 
-    public void flushToCanvasWrapper(int x, int y, int width, int height);
+    void flushToCanvasWrapper(int x, int y, int width, int height);
 
-    public int getWidth();
+    int getWidth();
 
-    public int getHeight();
+    int getHeight();
 
-    public void refreshResolution(int width, int height);
+    void refreshResolution(int width, int height);
 }
