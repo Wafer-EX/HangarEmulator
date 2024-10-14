8dc706d3c166cd8843330b33117ff8b57e34d3c6
Wafer-EX <minebuilder445@gmail.com>
2024-10-14 21:47:10 +0600

    Some improvements

--- src/main/java/aq/waferex/hangaremulator/ui/components/wrappers/HangarFormWrapper.java
+++ src/main/java/aq/waferex/hangaremulator/ui/components/wrappers/HangarFormWrapper.java
@@ -32,22 +32,32 @@ public class HangarFormWrapper extends HangarWrapper {
 
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.weightx = 1.0;
+        constraints.ipady = 12;
         constraints.gridy = 0;
 
-        for (var item : items) {
+        for (int i = 0; i < items.size(); i++) {
+            constraints.insets.set(i == 0 ? 4 : 2, 4, i == items.size() - 1 ? 4 : 2, 4);
+            var item = items.get(i);
+
             if (item instanceof StringItem stringItem) {
                 var label = new JLabel(stringItem.getText());
+
+                constraints.ipady = 8;
                 this.add(label, constraints);
             }
             else if (item instanceof ImageItem imageItem) {
                 // TODO: check it
                 var icon = new ImageIcon(imageItem.getImage().getBufferedImage());
                 var label = new JLabel(icon);
+
+                constraints.ipady = 0;
                 this.add(label, constraints);
             }
             else if (item instanceof TextField textField) {
                 var jTextField = new JTextField();
                 // TODO: influence to textField when type
+
+                constraints.ipady = 12;
                 this.add(jTextField, constraints);
             }
             else {
